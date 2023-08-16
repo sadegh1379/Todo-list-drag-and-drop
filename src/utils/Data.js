3d8132ff4b8dd1c925a8050eb8f667bf64b9a530
sadegh <akbarisadegh382@gmail.com>
2023-08-16 11:49:47 +0330

    fixed folders data

---
+++ src/utils/Data.js
@@ -0,0 +1,68 @@
+export const initialBoards = {
+  todo: {
+    id: "todo",
+    title: "Todo",
+    bgColor: "bg-light-red",
+    titleColor: "text-dark-red",
+    icon: "",
+    tasks: [
+      {
+        id: "20",
+        title:
+          "1Start with meditation, exercise & breakfast for a productive day",
+      },
+      {
+        id: "21",
+        title: "2Read to learn something new every day",
+      },
+      {
+        id: "22",
+        title: "3Read to learn something new every day",
+      },
+    ],
+  },
+  doing: {
+    id: "doing",
+    title: "Doing",
+    bgColor: "bg-light-pink",
+    titleColor: "text-dark-pink",
+    icon: "💪",
+    tasks: [
+      {
+        id: "23",
+        title:
+          "4Start with meditation, exercise & breakfast for a productive day",
+      },
+      {
+        id: "24",
+        title: "5Read to learn something new every day",
+      },
+      {
+        id: "25",
+        title: "6Read to learn something new every day",
+      },
+    ],
+  },
+  done: {
+    id: "done",
+    title: "Done",
+    bgColor: "bg-light-green",
+    titleColor: "text-dark-green",
+    icon: "🎉",
+    tasks: [
+      {
+        id: "26",
+        title:
+          "7Start with meditation, exercise & breakfast for a productive day",
+      },
+      {
+        id: "27",
+        title: "8Read to learn something new every day",
+      },
+      {
+        id: "28",
+        title: "9Read to learn something new every day",
+      },
+    ],
+  },
+};
