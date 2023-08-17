ef75d2e1d7fec31d9301f476bb9553e287a6c23f
sadegh <akbarisadegh382@gmail.com>
2023-08-18 01:45:59 +0330

    added react-scripts and test webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
-import App from "./src/App"
+import App from "./App"
 import './index.css';
 import 'react-edit-text/dist/index.css';
 
